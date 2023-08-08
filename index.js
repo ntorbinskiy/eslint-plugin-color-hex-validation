2c3a2b06e146334b0aee483e2c4a63a73bd2cb4f
NIKITA TORBINSKIY <n.torbinskiy@gmail.com>
2023-08-08 20:23:44 +0300

    Change rule to plugin

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
+
+
+
 module.exports = {
     meta: {
-        type: 'suggestion',
-        fixable: 'code',
+        name: "eslint-plugin-color-hex-validation",
+        version: "1.0.2"
     },
-    create(context) {
-        return {
-            Literal(node) {
-                if (
-                    typeof node.value === 'string' &&
-                    /^#([0-9a-fA-F]{3}){1,2}$/.test(node.value)
-                ) {
-                    const uppercaseValue = node.value.toUpperCase();
-                    if (uppercaseValue !== node.value) {
-                        context.report({
-                            node,
-                            message: 'CSS HEX value should be in uppercase.',
-                            fix(fixer) {
-                                return fixer.replaceText(node, `'${uppercaseValue}'`);
-                            },
-                        });
-                    }
-                }
+    rules: {
+        "color-hex-validation":{
+            meta: {
+                type: 'suggestion',
+                fixable: 'code',
             },
-        };
-    },
+            create(context) {
+                return {
+                    Literal(node) {
+                        if (
+                            typeof node.value === 'string' &&
+                            /^#([0-9a-fA-F]{3}){1,2}$/.test(node.value)
+                        ) {
+                            const uppercaseValue = node.value.toUpperCase();
+                            if (uppercaseValue !== node.value) {
+                                context.report({
+                                    node,
+                                    message: 'CSS HEX value should be in uppercase.',
+                                    fix(fixer) {
+                                        return fixer.replaceText(node, `'${uppercaseValue}'`);
+                                    },
+                                });
+                            }
+                        }
+                    },
+                };
+            },
+        },
+    }
 };
