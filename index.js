5d24d9cd47e6f9b442a8e56598a2875c6d9bef76
NIKITA TORBINSKIY <n.torbinskiy@gmail.com>
2023-08-08 20:06:24 +0300

    Git init

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+    meta: {
+        type: 'suggestion',
+        fixable: 'code',
+    },
+    create(context) {
+        return {
+            Literal(node) {
+                if (
+                    typeof node.value === 'string' &&
+                    /^#([0-9a-fA-F]{3}){1,2}$/.test(node.value)
+                ) {
+                    const uppercaseValue = node.value.toUpperCase();
+                    if (uppercaseValue !== node.value) {
+                        context.report({
+                            node,
+                            message: 'CSS HEX value should be in uppercase.',
+                            fix(fixer) {
+                                return fixer.replaceText(node, `'${uppercaseValue}'`);
+                            },
+                        });
+                    }
+                }
+            },
+        };
+    },
+};
